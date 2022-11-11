d6ef318a65a05d88f569a073c224eda75f3ad29d
Aman Kumar Sahani <71867895+amankumarsahani@users.noreply.github.com>
2022-11-11 14:25:31 +0530

    Whatsapp Message Sender to Number

---
+++ Whatsapp_Msg_Sender.py
@@ -0,0 +1,5 @@
+import pywhatkit
+from pywhatkit.main import print_sleep_time
+# pywhatkit.sendwhatmsg(phone_no='+919988025729',message='Hello!',time_hour=22,time_min=58,wait_time=40)
+# n=input("ENTER PHONE NUMBER")
+pywhatkit.sendwhatmsg_instantly('+917696309551','Hello')
